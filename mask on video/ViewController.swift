78cca32fdd37cfa2658481aea47c0af63682aada
ting3242006 <ting3242006@hotmail.com>
2022-01-30 21:54:02 +0800

    Initial Commit

---
+++ mask on video/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mask on video
+//
+//  Created by Ting on 2022/1/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
